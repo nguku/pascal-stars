16e4754824d97edc8bb43146c72ceb0969693408
evanson nguku n <ngukuevans@gmail.com>
2018-06-29 15:58:23 +0300

    Create assignment.js

---
+++ assignment.js
@@ -0,0 +1,9 @@
+/*
+write aprogram that converts centimeters to feet and inches for 
+values between 1meter  2 meters in steps of 5cm
+for values below 1ft wite short for values above 6ft write tall
+
+convert the values to the nearest inch
+*/
+
+//write a program to generate tommorrows date give the day to day
